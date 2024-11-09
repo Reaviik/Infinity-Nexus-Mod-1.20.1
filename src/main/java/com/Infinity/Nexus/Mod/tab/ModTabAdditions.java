864b96ee7545e5557919f7db018edf7d7789fa67
Reaviik <reavik_gamer@hotmail.com>
2024-11-09 16:56:37 -0300

    3.1.3-1.20.1 - Paxel fix by g-soldera
    Signed-off-by: Reaviik <reavik_gamer@hotmail.com>

--- src/main/java/com/Infinity/Nexus/Mod/tab/ModTabAdditions.java
+++ src/main/java/com/Infinity/Nexus/Mod/tab/ModTabAdditions.java
@@ -160,7 +160,7 @@ public class ModTabAdditions {
                         pOutput.accept(new ItemStack(ModBlocksAdditions.DEEPSLATE_INFINITY_ORE.get()));
                         //infinity tools
                         pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_SWORD.get()));
-                        //pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_PAXEL.get()));
+                        pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_PAXEL.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_HAMMER.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_PICKAXE.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_AXE.get()));
@@ -175,7 +175,7 @@ public class ModTabAdditions {
                         pOutput.accept(new ItemStack(ModItemsAdditions.INFINITY_BOOTS.get()));
                         //imperial tools
                         pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_SWORD.get()));
-                        //pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_PAXEL.get()));
+                        pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_PAXEL.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_HAMMER.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_PICKAXE.get()));
                         pOutput.accept(new ItemStack(ModItemsAdditions.IMPERIAL_INFINITY_AXE.get()));
